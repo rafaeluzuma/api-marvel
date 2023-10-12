1c62f21a97a5bfafc3f3d68da0aefb11e0fee75f
Rafael <rafaelrodrigues@bee.com.br>
2023-10-12 16:09:22 -0300

    b

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <main class="container-fluid px-0 d-flex flex-column vh-100">
+  <main class="container-fluid px-0 d-flex flex-column vh-100 overlay-scroll">
     <Navbar />
     
     <router-view></router-view>
@@ -11,5 +11,14 @@ import Navbar from './components/Navbar.vue';
 </template>
 
 <style scoped>
-
+  .overlay-scroll {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow-y: scroll;
+    z-index: 1000;
+    background: rgba(255, 255, 255, 0.9);
+  }
 </style>
