be58e79401b1aed6605b9ddfb0abc6fb05975310
Rafael <rafaelrodrigues@bee.com.br>
2023-10-12 16:02:51 -0300

    a

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <main class="container-fluid px-0 d-flex flex-column vh-100 overflow-x-hidden">
+  <main class="container-fluid px-0 d-flex flex-column vh-100">
     <Navbar />
     
     <router-view></router-view>
